ce9d6bdda37eac29711254b95a079b057f6d35ad
IhorRykun <IhorRykun@gmail.com>
2023-12-28 22:14:48 +0200

    add carusel

--- src/index.js
+++ src/index.js
@@ -2,3 +2,4 @@ import './js/button_wave';
 import './js/button__more';
 import './js/popap';
 import './js/modal';
+import './js/slick';
--- src/js/slick.js
+++ src/js/slick.js
@@ -1,26 +1,7 @@
 $('.carousel').slick({
-  // normal options...
-  infinite: false,
-
-  // the magic
-  responsive: [
-    {
-      breakpoint: 1024,
-      settings: {
-        slidesToShow: 3,
-        infinite: true,
-      },
-    },
-    {
-      breakpoint: 600,
-      settings: {
-        slidesToShow: 2,
-        dots: true,
-      },
-    },
-    {
-      breakpoint: 300,
-      settings: 'unslick', // destroys slick
-    },
-  ],
+  dots: false,
+  infinite: true,
+  speed: 500,
+  fade: true,
+  cssEase: 'linear',
 });
