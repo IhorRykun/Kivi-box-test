d4a1fe618983d6e53635c89f67f637576203c041
IhorRykun <IhorRykun@gmail.com>
2024-05-02 22:13:45 +0300

    update

--- src/js/button__more.js
+++ src/js/button__more.js
@@ -1,13 +1,31 @@
-(() => {
-  const container = document.querySelector('.whats__item__img--leptop');
-  const textSpan = document.querySelector('#span-more');
+const container1 = document.querySelector('.js-list-1');
+const container2 = document.querySelector('.js-list-2');
+const container3 = document.querySelector('.js-list-3');
+const textSpan1 = document.querySelector('#span-more1');
+const textSpan2 = document.querySelector('#span-more2');
+const textSpan3 = document.querySelector('#span-more3');
+// container.addEventListener('click', e => {
+//   const targetValue = textSpan.classList.toggle('display-nan');
+//   if (e.currentTarget.nodeName === 'BUTTON') {
+//     console.log(e.currentTarget.nodeName);
+//     return targetValue;
+//   }
+//   return;
+// });
 
-  container.addEventListener('click', e => {
-    const targetValue = textSpan.classList.toggle('display-nan');
-    if (e.currentTarget.nodeName === 'BUTTON') {
-      console.log(e.currentTarget.nodeName);
-      return targetValue;
-    }
-    return;
-  });
-})();
+container1.addEventListener('click', onClickOne);
+container2.addEventListener('click', onClickTwo);
+container3.addEventListener('click', onClickThree);
+function onClickOne() {
+  const targetValue = textSpan1.classList.toggle('display-nan');
+  return targetValue;
+}
+function onClickTwo() {
+  const targetValue = textSpan2.classList.toggle('display-nan');
+  return targetValue;
+}
+
+function onClickThree() {
+  const targetValue = textSpan3.classList.toggle('display-nan');
+  return targetValue;
+}
